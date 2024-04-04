4dc32e9182f06d9f7d817048aa8f33d248318a40
Boris Faure <boris@fau.re>
2024-04-04 23:50:58 +0200

    add examples/button_exti.rs

--- examples/button_exti.rs
+++ examples/button_exti.rs
@@ -3,8 +3,10 @@
 #![no_std]
 
 /// Set LED on when button is pressed, off when button is released.
+use defmt::info;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::{Level, Output, Pull, Speed};
 use embassy_stm32::Config;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -12,15 +14,19 @@ use {defmt_rtt as _, panic_probe as _};
 async fn main(_spawner: Spawner) {
     let config = Config::default();
     let p = embassy_stm32::init(config);
+    info!("Hello World!");
 
     let mut led = Output::new(p.PC13, Level::High, Speed::Low);
-    let button = Input::new(p.PA0, Pull::Up);
+    let mut button = ExtiInput::new(p.PA0, p.EXTI0, Pull::Up);
+
+    info!("Press the USER button...");
 
     loop {
-        if button.is_high() {
-            led.set_high();
-        } else {
-            led.set_low();
-        }
+        button.wait_for_rising_edge().await;
+        led.set_high();
+        info!("Pressed!");
+        button.wait_for_falling_edge().await;
+        info!("Released!");
+        led.set_low();
     }
 }
