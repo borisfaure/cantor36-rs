65f48ccc2e60953a26f331260b605b226b698a7c
Boris Faure <boris@fau.re>
2024-04-05 22:52:55 +0200

    examples/button_exti: fix button detection

--- examples/button_exti.rs
+++ examples/button_exti.rs
@@ -12,7 +12,31 @@ use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let config = Config::default();
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::{
+            mux, AHBPrescaler, APBPrescaler, Hse, HseMode, Pll, PllMul, PllPDiv, PllPreDiv,
+            PllQDiv, PllSource, Sysclk,
+        };
+        use embassy_stm32::time::Hertz;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(25_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll_src = PllSource::HSE;
+        config.rcc.pll = Some(Pll {
+            prediv: PllPreDiv::DIV25,
+            mul: PllMul::MUL336,
+            divp: Some(PllPDiv::DIV4), // 25mhz / 25 * 336 / 4 = 84Mhz.
+            divq: Some(PllQDiv::DIV7), // 25mhz / 25 * 336 / 7 = 48Mhz.
+            divr: None,
+        });
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV2;
+        config.rcc.apb2_pre = APBPrescaler::DIV1;
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.mux.clk48sel = mux::Clk48sel::PLL1_Q;
+    }
     let p = embassy_stm32::init(config);
     info!("Hello World!");
 
@@ -22,11 +46,11 @@ async fn main(_spawner: Spawner) {
     info!("Press the USER button...");
 
     loop {
-        button.wait_for_rising_edge().await;
-        led.set_high();
-        info!("Pressed!");
         button.wait_for_falling_edge().await;
-        info!("Released!");
+        info!("Pressed!");
         led.set_low();
+        button.wait_for_rising_edge().await;
+        info!("Released!");
+        led.set_high();
     }
 }
